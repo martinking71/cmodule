fbbdd53761b7ded7b34e53e76bd6ee7ec02b3b32
martinking71 <martinking71@gmail.com>
2022-06-30 11:04:01 +0100

    Update FindCURL.cmake

--- cmake/FindCURL.cmake
+++ cmake/FindCURL.cmake
@@ -4,7 +4,7 @@ set(CURL_ENABLE_EXPORT_TARGET OFF)
 set(BUILD_SHARED_LIBS ${CMODULE_SHARED_LIB})
 
 cmodule_add(
-  curl 7.80.0
-  URL "https://github.com/curl/curl/releases/download/curl-7_80_0/curl-7.80.0.tar.xz"
-  URL_HASH SHA256=a132bd93188b938771135ac7c1f3ac1d3ce507c1fcbef8c471397639214ae2ab
+  curl 7.84.0
+  URL "https://github.com/curl/curl/releases/download/curl-7_84_0/curl-7.84.0.tar.xz"
+  URL_HASH SHA256=a281cac88f36c268a6be1e3732b32db0eee5eb8fe3bd3e3cb774a2b045f43d01
 )
