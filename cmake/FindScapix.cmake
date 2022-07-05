458df5f2d4115e24a4c5b89bf30a98e75ccd131f
martinking71 <martinking71@gmail.com>
2022-07-05 10:43:02 +0100

    Update FindScapix.cmake

--- cmake/FindScapix.cmake
+++ cmake/FindScapix.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cmodule.cmake)
 
 cmodule_add(
-  scapix 1.0.35
-  URL "https://github.com/scapix-com/scapix/archive/refs/tags/v1.0.35.tar.gz"
-  URL_HASH SHA256=3eeabad538e9e06bc21f4ccafccfa97808b8458044ee32f28a37b81e7ce8d174
+  scapix 1.0.30
+  URL "https://github.com/scapix-com/scapix/archive/refs/tags/v1.0.30.tar.gz"
+  URL_HASH SHA256=030f5e7174114e62af30206f8376b1d3a05ee5b0cba88ac3f5d02a3e192ce1d5
 )
